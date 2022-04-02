752f53f072e80fcaa54a3655a6473b7397e6655b
Adrian <adriancosta1215@gmail.com>
2022-04-02 16:38:58 -0300

    Escolha de itens implementado

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
 function marcar(elementoClicado){
-    const prato = document.querySelector(".selec");
     
-    if (prato !== null){
+    console.log(elementoClicado)
+    
+    const pratoselecionado = document.querySelector(".selec");
+    
+    if (pratoselecionado !== null  && elementoClicado.classList.contains("prato")){
     
         
-        prato.classList.remove("selec") 
+        pratoselecionado.classList.remove("selec") 
         
     
     
     } 
+
+    elementoClicado.classList.add("selec"); 
+}
+
+
+function marcar2(elementoClicado){
+
+    console.log(elementoClicado)
+
+    const bebidaselecionada = document.querySelector(".bebida.selec");
     
+
+    if (bebidaselecionada !== null  && elementoClicado.classList.contains("bebida")){
+    
+        
+        bebidaselecionada.classList.remove("selec")
+        
+    
+    
+    } 
+
+
     elementoClicado.classList.add("selec"); 
     
-    
+}
+
+function marcar3(elementoClicado){
+
+    const sobremesaSelecionada = document.querySelector(".sobremesa.selec");
+
+    if(sobremesaSelecionada !== null && elementoClicado.classList.contains("sobremesa")){
+
+        sobremesaSelecionada.classList.remove("selec")
+
+
+    }
+
+    elementoClicado.classList.add("selec")
+
 }
 
